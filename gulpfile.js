6fed5d9e4e8b501dbffa4c67af8872383848a87d
(null) McNull <noreplies@hotmail.com>
2017-08-04 17:37:42 +0200

    smaller sheet

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
 var gulp = require('gulp');
 var replace = require('gulp-replace');
 
-gulp.task('svg-sprites', function() {
+gulp.task('svg-sprites', function () {
   return gulp.src([
-      'node_modules/material-design-icons/sprites/svg-sprite/*.svg',
-      '!node_modules/material-design-icons/sprites/svg-sprite/*-symbol.svg',
+    'node_modules/material-design-icons/sprites/svg-sprite/*.svg',
+    '!node_modules/material-design-icons/sprites/svg-sprite/*-symbol.svg',
   ])
-    .pipe(replace(/id=\"ic_([0-9a-zA-z_]+)_24px\"/g, function(pattern, replacement) {
-        var out = replacement.replace(/_/g,'-');
-        return 'id="' + out + '"';
+    // make id nice
+    .pipe(replace(/id=\"ic_([0-9a-zA-z_]+)_24px\"/g, function (pattern, replacement) {
+      var out = replacement.replace(/_/g, '-');
+      return 'id="' + out + '"';
     }))
+    // remove xml header
+    .pipe(replace(/<\?xml.+\?>/g, ''))
+    // remove attributes
+    .pipe(replace(/(width|height|viewBox|xmlns|xmlns\:xlink|x|y)=\"[^\"]*\"\ ?/g, ''))
+    // wrap inner svgs into defs
+    .pipe(replace(/(<svg[^>]*>)(.*)(<\/svg>)/, '$1<defs>$2</defs>$3'))
+    // trim white space
+    .pipe(replace(/\ >/g, '>'))
     .pipe(gulp.dest('dist'));
 });
